420a8dbbe8928fa283eaf6558ab54288a9e9342c
sehoon <gsh723@naver.com>
2021-05-13 14:36:51 +0900

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import './app.css';
+import Nav from './component/nav';
+
+function App() {
+  return (
+    <div className="container">
+     <Nav />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Nav = (props) => {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const dates = date.getDate();
+
+
+    const days = ['월요일','화요일','수요일','목요일','금요일','토요일','일요일' ];
+    const day = days[date.getDay()-1];
+
+    return(
+      <div className="nav">
+        <h1 className="todo-day">{year}년 {month+1}월 {dates}일</h1>
+        <p className="todo-date">{day}</p>
+        <p className="todo-left">할 일 2개 남음</p> 
+      </div>
+    );           
+};
+
+export default Nav;
